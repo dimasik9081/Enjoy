e2852e8fb200433c1adc5f973853055018760171
dm <dm@dm-home.(none)>
2016-04-25 22:55:53 +0400

    Новый пустой проект

---
+++ app/src/main/java/ru/enjoyflowers/shop/control/StartActivity.java
@@ -0,0 +1,15 @@
+package ru.enjoyflowers.shop.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.enjoyflowers.shop.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
